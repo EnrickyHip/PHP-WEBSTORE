9ad95f8ed828ac94dd8cd3c81efe1b72c07b2325
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-12-02 20:55:20 -0300

    configure env variables and database connection

---
+++ dotenv.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+define('HOST', $_ENV["HOST"]);
+define('DATABASE', $_ENV["DATABASE"]);
+define('USER', $_ENV["USER"]);
+define('PASSWORD', $_ENV["PASSWORD"]);
---
+++ src/models/Database.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Webstore\Models;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+require_once __DIR__ . "/../../dotenv.php";
+
+use PDO;
+
+class Database
+{
+  private static PDO $connection;
+
+  private const HOST = HOST;
+  private const NAME = DATABASE;
+  private const USER = USER;
+  private const PASSWORD = PASSWORD;
+
+  public static function getConnection(): PDO
+  {
+    if (!isset($connection)) {
+      try {
+        $dsn = 'mysql:host=' . self::HOST . ';dbname=' . self::NAME . ";charset=utf8mb4";
+        $attributes = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+        self::$connection = new PDO($dsn, self::USER, self::PASSWORD, $attributes);
+      } catch (\PDOException $exception) {
+        throw $exception->getMessage();
+      }
+    }
+
+    return self::$connection;
+  }
+}
